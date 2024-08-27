c9e078a64d5ac435647c74d0bef514e9a4cbc8c2
draphael89 <david@infinitygrowth.co>
2024-08-27 16:24:55 -0400

    remove .env.local dependencies - again

--- next.config.js
+++ next.config.js
@@ -1,17 +1,3 @@
-const path = require('path');
-const dotenv = require('dotenv');
-
-// Load .env.local file
-const envPath = path.join(__dirname, '.env.local');
-const result = dotenv.config({ path: envPath });
-
-if (result.error) {
-  throw result.error;
-}
-
-console.log('Env file loaded:', envPath);
-console.log('OPENAI_API_KEY defined:', !!process.env.OPENAI_API_KEY);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
