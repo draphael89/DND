5c3e97742ff95928c53b8f5e850c4ad0877c3d26
draphael89 <david@infinitygrowth.co>
2024-08-27 16:20:58 -0400

    remove .env.local dependencies - again

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
-    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
-  },
+  // Add any Next.js config options here if needed
 };
 
 export default nextConfig;
